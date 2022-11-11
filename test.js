19034f7679078348b547f06b1e56889c1c8f4e09
Kimsu <betnwjd25@gmail.com>
2022-11-11 10:40:33 +0900

    깃허브 테스트

---
+++ test.js
@@ -0,0 +1,12 @@
+let odd = 0;
+let even = 0;
+
+function test(){
+for(i=1; i < 51; i++){
+        if(1 % 2 ===0){
+            return  even += i;
+        }else{
+            return odd += i
+        }
+    }
+}
